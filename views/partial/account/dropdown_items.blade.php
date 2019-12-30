7ac3bd018db4e2f2d45b50c3ba29829f05332726
Alex Bouma <me@alexbouma.me>
2019-12-30 21:39:47 +0100

    Fix issues with duplicated dropdown dividers

--- views/partial/account/dropdown_items.blade.php
+++ views/partial/account/dropdown_items.blade.php
@@ -7,7 +7,7 @@
 @include('chief::partial.account.dropdown.support_items')
 <div class="dropdown-divider"></div>
 
+{{-- This is the only include that will include it's own `dropdown-divider` below --}}
 @include('chief::partial.account.dropdown.chief_items')
-<div class="dropdown-divider"></div>
 
 @include('chief::partial.account.dropdown.auth_items')

d9d29c0863e3542ca1ca695c1cbafb2296e1ca83
Alex Bouma <me@alexbouma.me>
2020-01-01 22:18:05 +0100

    Fix logic error, don't code at night ;)

--- src/Http/Middleware/SecurityHeaders.php
+++ src/Http/Middleware/SecurityHeaders.php
@@ -4,23 +4,23 @@ namespace IronGate\Chief\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Response;
 
 class SecurityHeaders
 {
     public function handle(Request $request, Closure $next)
     {
-        /** @var \Symfony\Component\HttpFoundation\Response $response */
+        /** @var \Illuminate\Http\Response $response */
         $response = $next($request);
 
         // Make sure we're dealing with what we think we are dealing with
-        if (!($response instanceof Response)) {
+        if ($response instanceof Response) {
             // Make sure there are no empty headers being set
             $headers = array_filter(config('chief.response.securityheaders', []));
 
             foreach ($headers as $header => $value) {
                 // Set the headers but do not replace them if already set
-                if ($response->headers->has($header)) {
+                if (!$response->headers->has($header)) {
                     $response->headers->set($header, $value);
                 }
             }

28352ec68b747f732336a8748aaa2ae7bcd4363d
Alex Bouma <me@alexbouma.me>
2020-01-16 11:28:29 +0100

    Extend UsesUUID trait with helpful query helpers

--- src/Concerns/UsesUUID.php
+++ src/Concerns/UsesUUID.php
@@ -4,6 +4,7 @@ namespace IronGate\Chief\Concerns;
 
 use Ramsey\Uuid\Uuid;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 trait UsesUUID
 {
@@ -11,6 +12,37 @@ trait UsesUUID
     {
         self::creating(function (Model $model) {
             $model->{$model->getUuidColumnName()} = Uuid::uuid4()->toString();
+        });
+    }
+
+    /**
+     * Find by the UUID attribute.
+     *
+     * @param string $uuid
+     *
+     * @return static|null
+     */
+    public static function findByUuid(string $uuid)
+    {
+        return (new static)->query()->uuid($uuid)->first();
+    }
+
+    /**
+     * Find or fail by the UUID attribute.
+     *
+     * @param string $uuid
+     *
+     * @return static
+     */
+    public static function findOrFailByUuid(string $uuid)
+    {
+        return (new static)->query()->uuid($uuid)->firstOrFail();
+    }
+
+    public function scopeUuid(Builder $query, string $uuid): void
+    {
+        $query->where(function (Builder $query) use ($uuid) {
+            $query->where($this->getUuidColumnName(), '=', $uuid);
         });
     }
 

dca7e1de985578ab15a1992d9a69442c4419a25f
Alex Bouma <me@alexbouma.me>
2020-10-13 13:34:20 +0200

    Fix WithMissingDirective for Lighthouse update

--- src/GraphQL/Directives/WithMissingDirective.php
+++ src/GraphQL/Directives/WithMissingDirective.php
@@ -2,56 +2,28 @@
 
 namespace IronGate\Chief\GraphQL\Directives;
 
-use Closure;
-use GraphQL\Deferred;
-use Illuminate\Database\Eloquent\Model;
-use GraphQL\Type\Definition\ResolveInfo;
-use Nuwave\Lighthouse\Schema\Values\FieldValue;
-use Nuwave\Lighthouse\Execution\DataLoader\BatchLoader;
-use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 use Nuwave\Lighthouse\Support\Contracts\FieldMiddleware;
 use Nuwave\Lighthouse\Support\Contracts\DefinedDirective;
-use Nuwave\Lighthouse\Schema\Directives\RelationDirective;
 use IronGate\Chief\GraphQL\Support\MissingRelationBatchLoader;
+use Nuwave\Lighthouse\Schema\Directives\WithRelationDirective;
 
-class WithMissingDirective extends RelationDirective implements FieldMiddleware, DefinedDirective
+class WithMissingDirective extends WithRelationDirective implements FieldMiddleware, DefinedDirective
 {
-    public function handleField(FieldValue $fieldValue, Closure $next): FieldValue
+    public function batchLoaderClass(): string
     {
-        $resolver = $fieldValue->getResolver();
+        return MissingRelationBatchLoader::class;
+    }
 
-        return $next(
-            $fieldValue->setResolver(function (Model $parent, array $args, GraphQLContext $context, ResolveInfo $resolveInfo) use ($resolver): Deferred {
-                $loader = BatchLoader::instance(
-                    MissingRelationBatchLoader::class,
-                    $resolveInfo->path,
-                    [
-                        'relationName'    => $this->directiveArgValue('relation', $this->nodeName()),
-                        'decorateBuilder' => function ($query) use ($resolveInfo) {
-                            $resolveInfo->argumentSet->enhanceBuilder(
-                                $query,
-                                $this->directiveArgValue('scopes', [])
-                            );
-                        },
-                    ]
-                );
-
-                return new Deferred(function () use ($loader, $resolver, $parent, $args, $context, $resolveInfo) {
-                    return $loader
-                        ->load($parent->getKey(), ['parent' => $parent])
-                        ->then(function () use ($resolver, $parent, $args, $context, $resolveInfo) {
-                            return $resolver($parent, $args, $context, $resolveInfo);
-                        });
-                });
-            })
-        );
+    public function relationName(): string
+    {
+        return $this->directiveArgValue('relation', $this->nodeName());
     }
 
     public static function definition(): string
     {
         return /** @lang GraphQL */ <<<'SDL'
 """
-Eager-load an Eloquent relation if it's missing.
+Eager-load a missing Eloquent relation.
 """
 directive @withMissing(
   """

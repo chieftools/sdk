2429e741f2ba81f13bfa3c390fc18ccf7b9e6e42
Alex Bouma <alex@bouma.me>
2021-10-31 14:51:09 +0100

    Update Tailwind components

--- views/tailwind/components/button.blade.php
+++ views/tailwind/components/button.blade.php
@@ -1,7 +1,33 @@
 @props([
     'href' => '#',
+    'size' => null,
+    'type' => null,
+    'icon' => null,
+    'iconType' => 'fal',
 ])
 
-<a href="{{ $href }}" class="group py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-brand-600 hover:bg-brand-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-500">
-    {{ $slot }}
+@php
+    $sizeClass = match($size) {
+        'xs' => 'px-2.5 py-1.5 rounded',
+        'sm' => 'px-3 py-2 rounded-md leading-4',
+        'lg' => 'px-6 py-3 rounded-md',
+        default => 'px-4 py-2 rounded-md',
+    };
+    $textClass = match($size) {
+        'xs' => 'text-xs',
+        'md', 'lg' => 'text-base',
+        default => 'text-sm',
+    };
+    $focusClass = 'focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-500';
+    $colorClass = match($type) {
+        'white' => 'text-gray-700 bg-white hover:bg-gray-50 border border-transparent',
+        'outline' => 'text-gray-700 bg-white hover:text-white hover:bg-brand-700 border border-brand-600',
+        default => 'text-white bg-brand-600 hover:bg-brand-700 border border-transparent',
+    };
+
+    $classes = "inline-flex {$sizeClass} {$textClass} {$focusClass} {$colorClass} font-medium shadow-sm";
+@endphp
+
+<a {{ $attributes->merge(['class' => $classes, 'href' => $href]) }}>
+    @if($icon)<i class="{{ $iconType }} fa-fw {{ $icon }} {{ $textClass }}"></i>&nbsp;@endif {{ $slot }}
 </a>
--- views/tailwind/components/panel.blade.php
+++ views/tailwind/components/panel.blade.php
@@ -1,22 +1,27 @@
 @props([
     'icon' => null,
+    'class' => null,
     'title' => null,
     'footer' => null,
+    'header' => null,
+    'iconType' => 'fad',
 ])
 
-<div class="shadow sm:rounded-md sm:overflow-hidden">
+<div class="shadow sm:rounded-md sm:overflow-hidden {{ $class ?? '' }}">
     @if($title)
         <div class="bg-white py-4 px-5 border-b border-gray-200 sm:px-6">
-            <h3 class="text-lg leading-6 font-medium text-gray-900">
-                @if($icon)<i class="fad fa-fw {{ $icon }}"></i> @endif{{ $title }}
+            <h3 class="inline-block text-lg leading-6 font-medium text-gray-900">
+                @if($icon)<i class="{{ $iconType }} fa-fw {{ $icon }}"></i> @endif{{ $title }}
             </h3>
+
+            {!! $header ?? '' !!}
         </div>
     @endif
     <div class="bg-white py-4 px-5">
         {{ $slot }}
     </div>
     @if($footer)
-        <div class="py-4 px-5 bg-gray-50 text-right">
+        <div class="flex gap-x-3 justify-end py-4 px-5 bg-gray-50">
             {{ $footer }}
         </div>
     @endif

1b2db07943d5cb2a852a5e634d9696f9cdbe15b5
Alex Bouma <alex@bouma.me>
2021-11-01 13:42:56 +0100

    Add more options to the button component

--- views/tailwind/components/button.blade.php
+++ views/tailwind/components/button.blade.php
@@ -3,29 +3,37 @@
     'size' => null,
     'type' => null,
     'icon' => null,
+    'disabled' => false,
     'iconType' => 'fal',
 ])
 
 @php
-    $sizeClass = match($size) {
-        'xs' => 'px-2.5 py-1.5 rounded',
-        'sm' => 'px-3 py-2 rounded-md leading-4',
-        'lg' => 'px-6 py-3 rounded-md',
-        default => 'px-4 py-2 rounded-md',
+    $classes = 'inline-flex font-medium shadow-sm ';
+
+    $classes .= match($size) {
+        'xs' => 'px-2.5 py-1.5 rounded ',
+        'sm' => 'px-3 py-2 rounded-md leading-4 ',
+        'lg' => 'px-6 py-3 rounded-md ',
+        default => 'px-4 py-2 rounded-md ',
     };
-    $textClass = match($size) {
-        'xs' => 'text-xs',
-        'md', 'lg' => 'text-base',
-        default => 'text-sm',
+    $classes .= match($size) {
+        'xs' => 'text-xs ',
+        'md', 'lg' => 'text-base ',
+        default => 'text-sm ',
     };
-    $focusClass = 'focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-500';
-    $colorClass = match($type) {
-        'white' => 'text-gray-700 bg-white hover:bg-gray-50 border border-transparent',
-        'outline' => 'text-gray-700 bg-white hover:text-white hover:bg-brand-700 border border-brand-600',
-        default => 'text-white bg-brand-600 hover:bg-brand-700 border border-transparent',
+    $classes .= $disabled ? '' : 'focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-500 ';
+    $classes .= match($type) {
+        'white' => 'text-gray-700 bg-white hover:bg-gray-50 border border-transparent ',
+        'outline' => 'text-gray-700 bg-white hover:text-white hover:bg-brand-700 border border-brand-600 ',
+        default => 'text-white bg-brand-600 hover:bg-brand-700 border border-transparent ',
     };
 
-    $classes = "inline-flex {$sizeClass} {$textClass} {$focusClass} {$colorClass} font-medium shadow-sm";
+    if ($disabled) {
+        $href    = '#';
+        $classes = str_replace('hover:', 'hover-disabled:', $classes);
+
+        $classes .= 'cursor-not-allowed ';
+    }
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes, 'href' => $href]) }}>

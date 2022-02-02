c05149ef40c6b21662b3b5c14ad589c172e3aba3
Alex Bouma <alex@bouma.me>
2022-02-02 12:50:00 +0100

    Improve default Sentry config

--- config/sentry.php
+++ config/sentry.php
@@ -1,9 +1,33 @@
 <?php
 
+$appNamespace = ucfirst(config('chief.namespace') ?? config('chief.id'));
+
 return [
+
     'dsn' => env('APP_DEBUG', false) ? null : env('SENTRY_PRIVATE_DSN', env('SENTRY_LARAVEL_DSN')),
 
     'release' => config('app.version'),
 
+    'error_types' => E_ALL ^ E_DEPRECATED ^ E_USER_DEPRECATED,
+
+    'traces_sampler' => [IronGate\Chief\Exceptions\Sentry::class, 'tracesSampler'],
+
+    'in_app_exclude' => [
+        base_path('vendor'),
+        app_path('Http/Middleware'),
+    ],
+
+    'in_app_include' => [
+        base_path('vendor/irongate'),
+    ],
+
     'send_default_pii' => false,
+
+    'class_serializers' => [
+        Illuminate\Queue\Jobs\Job::class          => [IronGate\Chief\Exceptions\Sentry::class, 'serializeJob'],
+        Illuminate\Database\Eloquent\Model::class => [IronGate\Chief\Exceptions\Sentry::class, 'serializeEloquentModel'],
+    ],
+
+    'controllers_base_namespace' => "IronGate\\{$appNamespace}\\Http\\Controllers",
+
 ];
---
+++ src/Exceptions/Sentry.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace IronGate\Chief\Exceptions;
+
+use Illuminate\Queue\Jobs\Job;
+use Sentry\Tracing\SamplingContext;
+use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\HttpFoundation\Request;
+
+class Sentry
+{
+    private const IGNORE_FROM_TRACING = [
+        'manifest.webmanifest',
+        'microsoft-identity-association.json',
+    ];
+
+    public static function tracesSampler(SamplingContext $context): float
+    {
+        $sampleRate = config('sentry.traces_sample_rate');
+
+        if ($sampleRate <= 0) {
+            return 0.0;
+        }
+
+        $request = request();
+
+        // We don't care about CORS requests
+        if ($request->isMethod(Request::METHOD_OPTIONS) && $request->headers->has('Access-Control-Request-Method')) {
+            return 0.0;
+        }
+
+        // We also don't care about some url's in this list so we don't trace them at all
+        if ($request->is(self::IGNORE_FROM_TRACING)) {
+            return 0.0;
+        }
+
+        // We always sample if the front-end indicates it was also sampled to have full traces front to back
+        if ($context->getParentSampled()) {
+            return 1.0;
+        }
+
+        return $sampleRate;
+    }
+
+    public static function serializeJob(Job $job): ?array
+    {
+        $data = [
+            'queue'    => $job->getQueue(),
+            'attempts' => $job->attempts(),
+        ];
+
+        // Extract tags if it's a Horizon job
+        if (method_exists($job, 'tags')) {
+            $data['tags'] = $job->tags();
+        }
+
+        return $data;
+    }
+
+    public static function serializeEloquentModel(Model $entity): ?array
+    {
+        return [$entity->getKeyName() => $entity->getKey()];
+    }
+}

13a9500159000a9b04d0b48a9ba4972d06dc0032
Alex Bouma <alex@bouma.me>
2022-03-18 13:52:39 +0100

    Fix CORS config

--- config/cors.php
+++ config/cors.php
@@ -24,11 +24,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        env('APP_DEBUG')
-            ? '*'
-            : config('app.domain'),
-    ],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 

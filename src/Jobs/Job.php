6a5c37cefaac2e6cce2370e60302b7881465e595
Alex Bouma <alex@bouma.me>
2022-07-27 14:54:30 +0200

    Add option to report usage async

--- src/Jobs/Job.php
+++ src/Jobs/Job.php
@@ -4,9 +4,8 @@ namespace ChiefTools\SDK\Jobs;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
 
 abstract class Job
 {
-    use Dispatchable, Queueable, InteractsWithQueue;
+    use Queueable, InteractsWithQueue;
 }

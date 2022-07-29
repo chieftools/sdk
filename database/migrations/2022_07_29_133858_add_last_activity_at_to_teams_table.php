7c1d6217e9ebf4cad24b96755dd64fabca26671b
Alex Bouma <alex@bouma.me>
2022-07-29 17:05:03 +0200

    Fix migration after column

--- database/migrations/2022_07_29_133858_add_last_activity_at_to_teams_table.php
+++ database/migrations/2022_07_29_133858_add_last_activity_at_to_teams_table.php
@@ -8,7 +8,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('teams', function (Blueprint $table) {
-            $table->dateTime('last_activity_at')->nullable()->after('limits');
+            $table->dateTime('last_activity_at')->nullable()->after('updated_at');
         });
     }
 };

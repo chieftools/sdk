c0923b2ba647a92410982df301a14d9988ff2759
Alex Bouma <alex@bouma.me>
2023-07-11 19:47:30 +0200

    Allow custom decision when to render support and analytics code

--- views/chief/partial/external.blade.php
+++ views/chief/partial/external.blade.php
@@ -1,2 +1,2 @@
-@includeWhen(config('services.plain.app_key') !== null && (auth()->guest() || auth()->user()->getPreference('enable_support_widget', true)), 'chief::partial.external.plain')
-@includeWhen(config('chief.analytics.fathom.site') !== null, 'chief::partial.external.fathom')
+@includeWhen(ChiefTools\SDK\Chief::shouldRenderSupportWidget(), 'chief::partial.external.plain')
+@includeWhen(ChiefTools\SDK\Chief::shouldRenderAnalyticsTracker(), 'chief::partial.external.fathom')

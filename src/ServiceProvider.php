d2c41f1e88dde398aa72b217848654ee74ad0716
Alex Bouma <alex@bouma.me>
2023-09-20 21:47:46 +0200

    Use Sentry lazy loading violation reporter

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,6 +8,7 @@ use RuntimeException;
 use ParagonIE\Certainty;
 use ChiefTools\SDK\API\Client;
 use Laravel\Passport\Passport;
+use Sentry\Laravel\Integration;
 use Illuminate\Auth\AuthManager;
 use Illuminate\Auth\RequestGuard;
 use ChiefTools\SDK\Http\Middleware;
@@ -332,7 +333,7 @@ class ServiceProvider extends IlluminateServiceProvider
         // In production we just report the violations instead of crashing the application since it's mostly performance issue not always a security issue
         if (app()->isProduction()) {
             Model::handleLazyLoadingViolationUsing(
-                static fn (Model $model, string $relation) => report(new LazyLoadingViolationException($model, $relation)),
+                Integration::lazyLoadingViolationReporter(),
             );
 
             Model::handleMissingAttributeViolationUsing(

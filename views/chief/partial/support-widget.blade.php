b2b6e996c90a8c2fa13eded633a7fd6df6a2514e
Alex Bouma <alex@bouma.me>
2023-11-04 15:38:12 +0100

    Load lazy components on hover in support widget

--- views/chief/partial/support-widget.blade.php
+++ views/chief/partial/support-widget.blade.php
@@ -1,8 +1,9 @@
-<div x-data="{ open: false, opened: false, showBugReportBtn: false }"
+<div x-data="{ open: false, loadLazy: false, showBugReportBtn: false }"
      x-on:click.away="open = false"
-     x-init="$watch('opened', () => showBugReportBtn = window.SENTRY_FEEDBACK && window.SENTRY_FEEDBACK.attachTo($refs.sentryBugReportBtn, {}) !== null)"
+     x-on:mouseover="loadLazy = true"
+     x-init="$watch('loadLazy', () => showBugReportBtn = window.SENTRY_FEEDBACK && window.SENTRY_FEEDBACK.attachTo($refs.sentryBugReportBtn, {}) !== null)"
      class="fixed bottom-6 right-6 z-50">
-    <button x-on:click="open = !open; opened = true" type="button" class="bg-brand hover:bg-brand-600 text-white px-2 py-1.5 rounded-md">
+    <button x-on:click="open = !open; loadLazy = true" type="button" class="bg-brand hover:bg-brand-600 text-white px-2 py-1.5 rounded-md">
         <i x-bind:class="{'opacity-0': open}" class="transition-opacity fad fa-fw fa-messages-question !absolute"></i>
         <i x-cloak x-bind:class="{'opacity-0': !open}" class="transition-opacity fa fa-fw fa-xmark !static"></i>
     </button>
@@ -28,7 +29,7 @@
 
             <hr class="my-2">
 
-            <template x-if="opened">
+            <template x-if="loadLazy">
                 <iframe src="https://status.chief.tools/badge?theme=light" sandbox="allow-popups allow-top-navigation-by-user-activation" class="ml-0.5" width="250" height="30" frameborder="0" scrolling="no"></iframe>
             </template>
         </div>

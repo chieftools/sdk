51d7491522c9e4df894743542cb24d6327dba739
Alex Bouma <alex@bouma.me>
2024-02-24 14:56:06 +0100

    Add descriptions to token prefixes

--- src/Enums/TokenPrefix.php
+++ src/Enums/TokenPrefix.php
@@ -11,6 +11,16 @@ enum TokenPrefix: string
     case OAUTH_ACCESS_TOKEN    = 'cto';
     case OAUTH_REFRESH_TOKEN   = 'ctr';
 
+    public function description(): string
+    {
+        return match ($this) {
+            self::TEAM_ACCESS_TOKEN     => 'Team Access Token',
+            self::PERSONAL_ACCESS_TOKEN => 'Personal Access Token',
+            self::OAUTH_ACCESS_TOKEN    => 'OAuth Access Token',
+            self::OAUTH_REFRESH_TOKEN   => 'OAuth Refresh Token',
+        };
+    }
+
     public function isForToken(RandomToken $token): bool
     {
         return $token->prefix === $this->value;

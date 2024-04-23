6858572b967bc6f4a403147171c2acb437a27a0c
Alex Bouma <alex@bouma.me>
2024-04-23 19:53:04 +0200

    Ensure session config is overridden correctly

--- config/session.php
+++ config/session.php
@@ -193,4 +193,17 @@ return [
 
     'same_site' => env('SESSION_SAME_SITE', 'lax'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Partitioned Cookies
+    |--------------------------------------------------------------------------
+    |
+    | Setting this value to true will tie the cookie to the top-level site for
+    | a cross-site context. Partitioned cookies are accepted by the browser
+    | when flagged "secure" and the Same-Site attribute is set to "none".
+    |
+    */
+
+    'partitioned' => env('SESSION_PARTITIONED_COOKIE', false),
+
 ];

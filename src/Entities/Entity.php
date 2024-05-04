8f0f44ebed7883a7cde51906c73a7da8fa29eac9
Alex Bouma <alex@bouma.me>
2024-05-04 15:43:26 +0200

    Disable queueable relations for all entities

--- src/Entities/Entity.php
+++ src/Entities/Entity.php
@@ -10,4 +10,10 @@ class Entity extends Eloquent
     use EloquentUserTimezones;
 
     protected $visible = ['id'];
+
+    public function getQueueableRelations(): array
+    {
+        // Queueable relations rarely have a benefit and mostly have negative effect, so we disable them by default
+        return [];
+    }
 }

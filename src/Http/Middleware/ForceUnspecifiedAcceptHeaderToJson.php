9ba56b7585d1752e85496765356e0d9aebd65f98
Alex Bouma <alex@bouma.me>
2025-01-21 09:41:19 +0100

    Fix JSON fallback middleware not working

--- src/Http/Middleware/ForceUnspecifiedAcceptHeaderToJson.php
+++ src/Http/Middleware/ForceUnspecifiedAcceptHeaderToJson.php
@@ -4,15 +4,32 @@ namespace ChiefTools\SDK\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\AcceptHeader;
 
 class ForceUnspecifiedAcceptHeaderToJson
 {
     public function handle(Request $request, Closure $next): mixed
     {
-        if ($request->acceptsAnyContentType()) {
+        if ($this->acceptsAnyContentType($request)) {
             $request->headers->set('Accept', 'application/json');
         }
 
         return $next($request);
+    }
+
+    private function acceptsAnyContentType(Request $request): bool
+    {
+        $acceptable = $this->getAcceptableContentTypes($request);
+
+        if (count($acceptable) === 0) {
+            return true;
+        }
+
+        return isset($acceptable[0]) && ($acceptable[0] === '*/*' || $acceptable[0] === '*');
+    }
+
+    private function getAcceptableContentTypes(Request $request): array
+    {
+        return array_map('strval', array_keys(AcceptHeader::fromString($request->header('Accept'))->all()));
     }
 }

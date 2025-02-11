3007e69d00cdf50065a570834f80f6378c82df84
Alex Bouma <alex@bouma.me>
2025-02-11 16:27:20 +0100

    Use the apps default layout as a base for error pages

--- views/chief/errors/base.blade.php
+++ views/chief/errors/base.blade.php
@@ -1,4 +1,4 @@
-@extendsfirst(['layout.html', 'chief::layout.html'], ['bodyClass' => $bodyClass ?? '', 'fullHeight' => true])
+@extendsfirst(['layout.default', 'chief::layout.html'], ['bodyClass' => $bodyClass ?? '', 'fullHeight' => true])
 
 @section('body')
     <div class="min-h-full pt-16 pb-12 flex flex-col bg-white">

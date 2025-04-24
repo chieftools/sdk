a29865bb33db913210af23b8b74e2de399c34e57
Alex Bouma <alex@bouma.me>
2025-04-24 16:07:24 +0200

    Try to stay on the same route when switching teams

--- src/Http/Controllers/Team/SwitchActive.php
+++ src/Http/Controllers/Team/SwitchActive.php
@@ -2,8 +2,10 @@
 
 namespace ChiefTools\SDK\Http\Controllers\Team;
 
+use Illuminate\Http\Request;
 use ChiefTools\SDK\Entities\Team;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Route;
 
 class SwitchActive
 {
@@ -12,6 +14,21 @@ class SwitchActive
         $user = authenticated_user_or_fail();
 
         $user->setCurrentTeam($team);
+
+        $previousUrl = session()->previousUrl();
+
+        if ($previousUrl !== null) {
+            $previousRequest = Request::create($previousUrl);
+
+            $route = Route::getRoutes()->match($previousRequest);
+
+            // To prevent redirecting to a 404 page we check if the route is a generic route without any additional parameters next to the team hint
+            if (count($route->parameters()) === 1 && $route->parameter('team_hint') !== null) {
+                return to_route($route->getName(), [
+                    'team_hint' => $team->slug,
+                ]);
+            }
+        }
 
         return redirect()->to(home());
     }

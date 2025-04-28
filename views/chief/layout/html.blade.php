3344aa3e148f8943fa5ac95e1f862c8c7edd930b
Alex Bouma <alex@bouma.me>
2025-04-28 16:54:25 +0200

    Allow overrides of styles and scripts sections

--- views/chief/layout/html.blade.php
+++ views/chief/layout/html.blade.php
@@ -4,7 +4,7 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}" class="antialiased {{ ($fullHeight ?? false) === true ? 'h-full' : '' }}">
     <head>
-        <meta charset="utf-8"/>
+        <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="theme-color" content="{{ config('chief.brand.color', '#2ecc71') }}">
@@ -31,13 +31,15 @@
         <link rel="icon" href="{{ static_asset('icons/' . config('chief.id') . '_favicon.svg') }}" type="image/svg+xml">
         <link rel="alternate icon" href="{{ static_asset('icons/' . config('chief.id') . '_favicon.ico') }}" sizes="32x32">
 
-        @section('styles')
+        @hasSection('styles')
+            @yield('styles')
+        @else
             @if(config('chief.assets.provider') === 'mix')
                 <link media="all" type="text/css" rel="stylesheet" href="{{ asset(mix('css/app.min.css')) }}">
             @else
                 @vite('resources/css/app.less')
             @endif
-        @show
+        @endif
 
         {!! Sentry\Laravel\Integration::sentryMeta() !!}
 
@@ -50,13 +52,15 @@
             @yield('body')
         </div>
 
-        @section('scripts')
+        @hasSection('scripts')
+            @yield('scripts')
+        @else
             @if(config('chief.assets.provider') === 'mix')
                 <script src="{{ asset(mix('js/app.min.js')) }}"></script>
             @else
                 @vite('resources/js/app.js')
             @endif
-        @show
+        @endif
 
         @stack('body.script')
 

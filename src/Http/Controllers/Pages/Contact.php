4f1b5015d3266edbbcb53947030fd52a873fb893
Alex Bouma <alex@bouma.me>
2025-07-16 11:24:45 +0200

    Merge query params with redirect urls

--- src/Http/Controllers/Pages/Contact.php
+++ src/Http/Controllers/Pages/Contact.php
@@ -2,12 +2,13 @@
 
 namespace ChiefTools\SDK\Http\Controllers\Pages;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 
 class Contact
 {
-    public function __invoke(): RedirectResponse
+    public function __invoke(Request $request): RedirectResponse
     {
-        return redirect()->away(chief_site_url('contact'), 301);
+        return redirect()->away(chief_site_url('contact', query: $request->query()), 301);
     }
 }
--- src/Http/Controllers/Pages/Privacy.php
+++ src/Http/Controllers/Pages/Privacy.php
@@ -2,12 +2,13 @@
 
 namespace ChiefTools\SDK\Http\Controllers\Pages;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 
 class Privacy
 {
-    public function __invoke(): RedirectResponse
+    public function __invoke(Request $request): RedirectResponse
     {
-        return redirect()->away(chief_site_url('privacy'), 301);
+        return redirect()->away(chief_site_url('privacy', query: $request->query()), 301);
     }
 }

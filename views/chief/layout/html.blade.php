50fafbb68204dcf3882672d59373a44e1c9c37b0
Alex Bouma <alex@bouma.me>
2026-08-25 16:02:16 +0200

    Allow flag to remove default favicon rendering

--- views/chief/layout/html.blade.php
+++ views/chief/layout/html.blade.php
@@ -59,8 +59,10 @@
             @endif
         @endif
 
-        <link rel="icon" href="{{ static_asset('icons/' . config('chief.id') . '_favicon.svg') }}" type="image/svg+xml">
-        <link rel="alternate icon" href="{{ static_asset('icons/' . config('chief.id') . '_favicon.ico') }}" sizes="32x32">
+        @if(!($noFavicon ?? false))
+            <link rel="icon" href="{{ static_asset('icons/' . config('chief.id') . '_favicon.svg') }}" type="image/svg+xml">
+            <link rel="alternate icon" href="{{ static_asset('icons/' . config('chief.id') . '_favicon.ico') }}" sizes="32x32">
+        @endif
 
         <link rel="stylesheet" href="{{ url('/chief/navigate-track.css') }}?id={{ rawurlencode((string)config('app.version', '@dev')) }}" data-navigate-track>
 
